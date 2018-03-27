780e8207e64a6cf65327af15a6c7d6fb891e1f40
Jason Yoo <jasonyoo0116@gmail.com>
2018-03-27 02:14:38 -0700

    v1 complete with setup, alias, goto, back, list, help

---
+++ README.md
@@ -0,0 +1,9 @@
+dmover is a shell program that simplifies directory traversal in the command line.
+
+Getting Started:
+- Download the repository through Github
+- Enter dmover's directory
+- Run `sh dmover.sh setup && source ~/.bash_profile`
+
+Availabe Commands
+- Please run `dmover.sh help after the setup process for more info.`
--- dmover.sh
+++ dmover.sh
@@ -1,91 +1,75 @@
 #!/bin/sh
 
-function help_text() {
-	echo 
-	echo "---------------------------------------------------"
-	echo "Availabe Commands:"
-	echo "---------------------------------------------------"
-	echo "setup: Run this command before you use the program."
-	echo "alias: Run this command in a directory to set an alias for that directory to be used with goto."
-	echo "list: Run this command to view all recorded aliases."
-	echo "goto: Run this command to access a directory through its alias from anywhere."
-	echo "back: Run this command to return to the last directory accessed through dmover."
-	echo "run-from: Run this command to execute an arbitrary shell command in a directory of your choosing."
-	echo
-}
+if [ -f ~/.dmoverrc ]; then
+	while read -r line; do
+		ALIAS_NAME="$(echo $line | cut -d '=' -f1)"
+		ADDRESS="$(echo $line | cut -d '=' -f2)"
+		alias goto-${ALIAS_NAME}="pushd ${ADDRESS}"
+	done < ~/.dmoverrc
+	alias back="popd 2>/dev/null || dmover directory stack is empty."
+fi
 
 case "$1" in
-	# Setup: Create a
-	# Cause .bash_profile to load .dmoverrc list of aliases
-	# Create an alias in .bash_profile that reloads .bash_profile after opening it
 	"setup")
 		if [ ! -f ~/.dmoverrc ]; then
-			echo '#!/bin/sh' >> ~/.dmoverrc
-			chmod +x ~/.dmoverrc
-			echo "~/.dmoverrc" >> ~/.bash_profile
+			touch ~/.dmoverrc
+			#chmod +x ~/.dmoverrc
+			echo "#---------------------------------------------------" >> ~/.bash_profile
+			echo "# dmover.sh related config" >> ~/.bash_profile
+			echo "#---------------------------------------------------" >> ~/.bash_profile
+			echo "PATH=$PATH:~/"
+			echo "source ~/dmover.sh" >> ~/.bash_profile
+			cp dmover.sh ~/
+			chmod +x ~/dmover.sh
 			echo "Setup is complete."
-			echo "Type 'source .bash_profile' in the command line and you're good to go!"
+			echo "If you haven't, type 'source ~/.bash_profile' in the command line and you're good to go!"
 		else
 			echo "You are already set up and good to go!"
+			echo "Type 'dmover.sh help' in the command line if you need help with the program."
 		fi
 		;;
 	"alias")
 		if [ -z "$2" ]; then
 			echo "You must state the name of the alias."
-			echo "Example Usage: dmover alias ALIAS_NAME"
+			echo "  - Example: dmover.sh alias ALIAS_NAME"
 			exit 1
-		fi
-		echo "$2:$(pwd)" >> .dmoverrc
-		echo "Alias for $(pwd) set to $2."
+		elif [ -n "$(echo '$2' | grep ' ')" ]; then
+			echo "Alias name cannot contain white spaces."
+		else
+	        echo "$2"="$(pwd)" >> ~/.dmoverrc
+	        echo "Alias for $(pwd) set to $2."
+	        echo "This alias will take effect on new command line tabs."
+	    fi
 		;;
 	"list")
-		if [ ! -f ".dmoverrc" ]; then
-			echo "There aren't any aliases set. Please use the alias command first."
-			exit 1
-		fi
 		echo
 		echo "---------------------------------------------------"
 		echo "Recorded Aliases"
 		echo "---------------------------------------------------"
 		while read -r line; do
 			echo "$line"
-		done < ".dmoverrc"
+		done < ~/.dmoverrc
 		echo
 		;;
-	"goto")
-		if [ -z "$2" ]; then
-			echo "You must state the alias name or path to traverse to."
-			echo "Example Usage 1: dmover goto ALIAS_NAME"
-			echo "Example Usage 2: dmover goto /home/dev/code/project1"
-			exit 1
-		fi
-		if [ -d "$2" ]; then
-			pushd "$2"
-			exit 0
-		fi
-		if [ ! -f ".dmoverrc" ]; then
-			echo "There aren't any aliases set. Please use the alias command first."
-			exit 1
-		fi
-		while read -r line; do
-			ALIAS_NAME="$(echo \"$line\" | cut -d ':' -f1)"
-			ADDRESS="$(echo \"$line\" | cut -d ':' -f2)"
-			if [ "$2" = "$ALIAS_NAME" ]; then
-				pushd "$ADDRESS" 1>/dev/null 2>/dev/null
-				exit 0
-			fi
-		done < ".dmoverrc"
-		echo "No alias or valid directory path has been found."
-		exit 1
-		;;
-	"back")
-		echo "Returning to the directory: $(popd 2>/dev/null)"
-		popd 1>/dev/null 2>/dev/null
-		;;
-	"run-from")
-
+	"help")
+		echo 
+		echo "---------------------------------------------------"
+		echo "Availabe Commands:"
+		echo "---------------------------------------------------"
+		echo "setup: Run this command before you use the program."
+		echo "  - Example: sh dmover.sh setup && source .bash_profile"
+		echo "alias: Run this command in a directory to set an alias for that directory to be used with goto."
+		echo "  - Example: dmover.sh alias ALIAS_NAME"
+		echo "list: Run this command to view all recorded aliases."
+		echo "  - Example: dmover.sh list"
+		echo "goto: Run this command as a standalone to access a directory through its alias from anywhere."
+		echo "  - Example: goto-ALIAS_NAME"
+		echo "back: Run this command as a standalone to return to the last directory accessed through dmover."
+		echo "  - Example: back"
+		echo "PS: dmover will copy itself to its user's home directory. Please do not remove the copy or dmover won't work."
+		echo
 		;;
 	*)
-		help_text
+		echo "dmover is online. Run 'dmover.sh help' for more info."
 		;;
 esac
